c53ce8991e1184c8eddeaf5875b9e8e72c392820
Stinky-c <60587749+Stinky-c@users.noreply.github.com>
2026-04-03 07:49:43 -0700

    Add web build

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
-use eframe::NativeOptions;
-use std::path::PathBuf;
-
+/*
 #[cfg(feature = "talc")]
 use talc::{source::Claim, *};
 
 #[cfg(all(feature = "talc", feature = "mimalloc"))]
 compile_error!("Only one allocator can be enabled. Pick between 'mimalloc' or 'talc'");
 
-#[cfg(feature = "mimalloc")]
+#[cfg(all(feature = "mimalloc",not(target_arch = "wasm32")))]
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
-#[cfg(feature = "talc")]
+#[cfg(all(feature = "talc",not(target_arch = "wasm32")))]
 #[global_allocator]
 static TALC: TalcLock<spinning_top::RawSpinlock, Claim> = TalcLock::new(unsafe {
     static mut INITIAL_HEAP: [u8; min_first_heap_size::<DefaultBinning>() + 100000] =
@@ -19,19 +17,21 @@ static TALC: TalcLock<spinning_top::RawSpinlock, Claim> = TalcLock::new(unsafe {
 
     Claim::array(&raw mut INITIAL_HEAP)
 });
+ */
 
 const APP_NAME: &str = concat!("com.buckydev.", env!("CARGO_PKG_NAME"));
 mod app;
 pub mod carditems;
 pub mod components;
 
+#[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result {
+    use std::path::PathBuf;
+
     egui_logger::builder().init().unwrap();
-    let options = NativeOptions {
+    let options = eframe::NativeOptions {
         persist_window: true,
-        persistence_path: Some(PathBuf::from(
-            r#"./config/hello.ron"#,
-        )),
+        persistence_path: Some(PathBuf::from(r#"./config/hello.ron"#)),
         ..Default::default()
     };
 
@@ -40,4 +40,50 @@ fn main() -> eframe::Result {
         options,
         Box::new(|cc| Ok(Box::new(app::App::new(cc)))),
     )
+}
+
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    use eframe::wasm_bindgen::JsCast as _;
+
+    // Redirect `log` message to `console.log` and friends:
+    egui_logger::builder().init().unwrap();
+
+    let web_options = eframe::WebOptions::default();
+
+    wasm_bindgen_futures::spawn_local(async {
+        let document = web_sys::window()
+            .expect("No window")
+            .document()
+            .expect("No document");
+
+        let canvas = document
+            .get_element_by_id("the_canvas_id")
+            .expect("Failed to find the_canvas_id")
+            .dyn_into::<web_sys::HtmlCanvasElement>()
+            .expect("the_canvas_id was not a HtmlCanvasElement");
+
+        let start_result = eframe::WebRunner::new()
+            .start(
+                canvas,
+                web_options,
+                Box::new(|cc| Ok(Box::new(app::App::new(cc)))),
+            )
+            .await;
+
+        // Remove the loading text and spinner:
+        if let Some(loading_text) = document.get_element_by_id("loading_text") {
+            match start_result {
+                Ok(_) => {
+                    loading_text.remove();
+                }
+                Err(e) => {
+                    loading_text.set_inner_html(
+                        "<p> The app has crashed. See the developer console for details. </p>",
+                    );
+                    panic!("Failed to start eframe: {e:?}");
+                }
+            }
+        }
+    });
 }
